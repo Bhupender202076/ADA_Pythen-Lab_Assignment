f1a581732540f7a2cdd062965b31e31f0f142522
BHUPENDER <91334105+Bhupender202076@users.noreply.github.com>
2022-11-24 23:22:42 +0530

    Create N-queen backtracking.py

---
+++ N-queen backtracking.py
@@ -0,0 +1,57 @@
+
+#Backtracking for  N-Queens
+n=4
+ans=[]
+board=[[0]*n for _ in range(n)]
+#print(board)
+
+def isSafe(board,i,j):
+    for k in range(j):                #check condition for row
+        if board[i][k]==1:
+            return False
+    for k in range(i):                  #check condition for column
+        if board[k][j]==1:
+            return False
+    row=i
+    col=j 
+    while (row>=0 and col>=0):
+        if board[row][col]==1:
+            return False
+        row=row-1
+        col=col-1    
+    while (i<n and j>=0):
+        if board[i][j]==1:
+            return False
+        i=i+1
+        j=j-1    
+    return True        
+
+def saveSol(board,ans):
+    temp=[]
+    for i in range(n):
+        for j in range(n):
+            temp.append(board[i][j])
+    ans.append(temp)
+       
+
+def solve(board,ans,n,j):
+    if j==n:
+        saveSol(board,ans)
+        return 
+    for i in range(n):
+        if isSafe(board,i,j):
+            board[i][j]=1
+            solve(board,ans,n,j+1)
+            board[i][j]=0
+
+def display():
+    for i in range(len(ans)):
+        print(ans[i],end=" ")
+        print("\n")    
+        board=[[0,0,0,0],
+               [0,0,0,0],
+               [0,0,0,0],
+               [0,0,0,0]]
+
+solve(board,ans,n,0)
+display()
